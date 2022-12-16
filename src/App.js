d0b5b2a8e066a127cd0f4608e5f95617e46ec12b
Aartisoni3515 <aartisoni3517@gmail.com>
2022-12-16 16:28:46 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -6,28 +6,35 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Services from "./components/Services";
+// import CartList from "./components/CartList"
 import Cart from "./components/Cart";
-// import NavBar from "./components/NavBar";
+ 
 
 function App() {
   return (
     <>
       <div className="nav-bar">
+     
         <Router>
-          {/* <NavBar /> */}
           
           <Routes>
             <Route path="/"   element={<Home />} />
             <Route path="/Services" element={<Services />} />
             <Route path="/About" element={<About />} />
             <Route path="/Contact" element={<Contact />} />
+            {/* <Route path="/Cart" element={<Cart />} /> */}
             <Route path="/Cart" element={<Cart />} />
+            <Route path="/services" element={<Cart />} />
+
+            {/* <Route path="/services/:id" element={<Cart />} /> */}
+            
 
 
           </Routes>
     
         </Router>
       </div>
+      
     </>
   );
 }
