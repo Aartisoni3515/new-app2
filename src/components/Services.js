576f2163bf995ecdcd7f01299f94577c7d3c2db4
Aartisoni3515 <aartisoni3517@gmail.com>
2023-02-01 12:18:57 +0530

    change

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 class Services extends Component {
-    render() {
-        return (
-          <>
-            <Carousel  autoPlay={true}>
-                <div>
-                    <img src="../Image/img2.jpg" />
-                </div>
-                <div>
-                <img src="../Image/img1.jpg" />
-                   
-                </div>
-                <div>
-                <img src="../Image/img3.jpg" />
+  render() {
+    return (
+      <>
+        <Carousel autoPlay={true}>
+          <div className="car">
+            <div>
+              <img src="../Image/img2.jpg" />
+            </div>
+            <div>
+              <img src="../Image/img1.jpg" />
+            </div>
+            <div>
+              <img src="../Image/img3.jpg" />
+            </div>
+            <div>
+              <img src="../Image/img4.png" />
+            </div>
+          </div>
+        </Carousel>
+      </>
+    );
+  }
+}
 
-                </div>
-                <div>
-                <img src="../Image/img4.png" />
-
-                </div>
-            </Carousel>
-            </>
-        );
-    }
-};
- 
 export default Services;
